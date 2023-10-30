b74d7233d31fa92cb15b16059ff3893c146600a9
damates <kich_alex654@mail.ru>
2023-10-30 20:10:32 +0300

    Initial commit

---
+++ src/main/java/org/example/AsAddrHierarchy.java
@@ -0,0 +1,14 @@
+import java.util.Date;
+
+public class AsAddrHierarchy {
+    private long id;
+    private long objectId;
+    private long parentObjId;
+    private long changeId;
+    private int prevId;
+    private int nextId;
+    private Date updateDate;
+    private Date startDate;
+    private Date endDate;
+    private int isActive;
+}
---
+++ src/main/java/org/example/AsAddrObj.java
@@ -0,0 +1,20 @@
+import java.util.Date;
+
+public class AsAddrObj {
+    private long id;
+    private long objectId;
+    private String objectGUID;
+    private long changeId;
+    private String name;
+    private String typeName;
+    private int level;
+    private int operTypeId;
+    private int prevId;
+    private int nextId;
+    private Date updateDate;
+    private Date startDate;
+    private Date endDate;
+    private int isActual;
+    private int isActive;
+
+}
